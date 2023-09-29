5f4d4cca5ade12ba53e69dcee3cb173a0db8c9e4
Augusto008 <aquila.real0@gmail.com>
2023-09-28 21:14:45 -0300

    refactor: creating a standard validation rule
    Changing the validation format of various functions to a singular standard validation function of RegExp class.

--- src/util/matchPattern/regExp.js
+++ src/util/matchPattern/regExp.js
@@ -2,16 +2,27 @@ import Joi from "joi";
 
 export default  class RegExp {
 
-    async nameMatch(data) {
-
+    async match(data) {
         try {
-            const match = Joi.string().pattern(/^[\p{L}\p{N}\s]+$/u).min(3).max(255).required();
-            await match.validateAsync(data);
+            
+            const match = Joi.object({
+                name: Joi.string().pattern(/^[\p{L}\p{N}\s]+$/u).min(3).max(255),
+                email: Joi.string().email(),
+                password: Joi.string(),
+            });
+            
+            let test;
+
+            for (const check in data) {
+                if (match.describe().keys[check]) {
+                    await match.validateAsync({ [check]: data[check] }).catch(error => { throw new Error (`invalide type: ${check}`); });
+                };
+            }
+
             return { success: true };
         } catch (error) {
-            return { success: false, message: "Invalid name" };
+            return { success: false, message: error.message };
         }
-
     }
 
 }
