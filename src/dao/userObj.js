a79dc62d6b35e78b00f6571b95a6e63142926c83
Augusto008 <aquila.real0@gmail.com>
2023-09-28 21:26:01 -0300

    feat: create userObj.js
    For now, contains just the create user and its relations function. If necessary, I'll add more functions specific for the users table.

---
+++ src/dao/userObj.js
@@ -0,0 +1,73 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default class UserDAO {
+
+    async checkRelations(data) {
+
+        try {
+            let result = await prisma.$queryRaw`WITH 
+                    company_result AS (
+                        SELECT id, name FROM companies WHERE id = ${data.company}
+                    ),
+                    role_result AS (
+                        SELECT id, role FROM roles WHERE id = ${data.role}
+                    )
+                SELECT 
+                    company_result.id as id_companies, company_result.name as company_name, role_result.id as id_roles, role_result.role as role_name
+                FROM 
+                    company_result, role_result
+                WHERE
+                    company_result.id IS NOT NULL AND role_result.id IS NOT NULL
+                UNION ALL
+                SELECT 
+                    NULL, NULL, NULL, NULL
+                WHERE 
+                    NOT EXISTS (SELECT 1 FROM company_result) 
+                    OR NOT EXISTS (SELECT 1 FROM role_result);`;
+
+            if (!result[0] || !result[0].id_companies || !result[0].id_roles) {
+                return { success: false };
+            };
+
+            return { success: true, message: result[0] };
+        } catch (error) {
+            return { success: false, message: error.message };
+        }
+
+    }
+
+    async create(data) {
+        try {
+            const result = await prisma.$transaction(async (prisma) => {
+                const user = await prisma.users.create({
+                    data: {
+                        name: data.name,
+                        email: data.email,
+                        password: data.password,
+                    }
+                });
+            
+                const relations = await prisma.users_companies.create({
+                    data: {
+                        id_users: user.id,
+                        id_companies: data.id_companies,
+                        id_roles: data.id_roles,
+                    }
+                });
+            
+                return { user, relations };
+            });
+            
+            if (!result.user || !result.relations) {
+                return { success: false, message: 'Error creating user' };
+            }
+            
+            return { success: true, message: result };
+        } catch (error) {
+            return { success: false, message: error.message };
+        }
+    }
+
+}
