ad78eda877ad9951f8f42eda06e3b2bd839e0ddf
Augusto008 <aquila.real0@gmail.com>
2023-10-02 22:23:43 -0300

    refactor: remove the FKs use in validation
    Remove the FKs use in validation and change the "many" function test to "result.length" format.

--- src/util/validation/validateUser.js
+++ src/util/validation/validateUser.js
@@ -11,21 +11,18 @@ export default class UserValidator {
     async validateUserPOST(user) {
 
         try {
-            const { name, email, password } = user;
-            const add = { company: Number(user.id_companies), role: Number(user.id_roles), name, email, password };
-
-            const checkRelations = await actionUser.checkRelations(add);
+            const checkRelations = await actionUser.checkRelations(user);
             if (!checkRelations.success) {
                 return { success: false, message: "invalid parameters" };
             };
 
-            const match = await regExp.match(add);
+            const match = await regExp.match(user);
             if(!match.success) {
                 return { success: false, message: match.message };
             };
 
-            let exist = await actionDB.many({ email });
-            if (exist.success) {
+            let exist = await actionDB.many('users', { email: user.email });
+            if (exist.result.length > 0) {
                 return { success: false, message: "This email is already in use" };
             };
 
@@ -38,8 +35,7 @@ export default class UserValidator {
 
     async validateUserUPDATE(user) {
         try {
-            const {id} = user;
-            const actualUser = await actionDB.unique('users', {id: Number(id)}, user.toUpdate);
+            const actualUser = await actionDB.unique('users', {id: Number(user.id)});
             if (!actualUser) {
                 return { success: false, message: "incorrect Parameters" };
             }
@@ -49,9 +45,14 @@ export default class UserValidator {
                 return { success: false, message: match.message };
             }
 
+            let exist = await actionDB.many('users', { email: user.email });
+            if (exist.result.length > 0) {
+                return { success: false, message: "This email is already in use" };
+            };
+
             return { success: true }
         } catch (error) {
-            return { success: false, message: error.message };            
+            return { success: false, message: error.message };
         }
     }
     
