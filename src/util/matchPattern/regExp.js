a75421ba05ad84ecd48ecb925aacb9677018c521
Augusto008 <aquila.real0@gmail.com>
2023-10-02 21:27:59 -0300

    feat ('regExp.js'): add more validation rules

--- src/util/matchPattern/regExp.js
+++ src/util/matchPattern/regExp.js
@@ -6,16 +6,25 @@ export default  class RegExp {
         try {
             
             const match = Joi.object({
+                contact: Joi.string(),
+                body: Joi.string(),
+                description: Joi.string(),
+                email: Joi.string().email().max(100),
+                footer: Joi.any().optional(),
+                logo: Joi.string().max(50),
                 name: Joi.string().pattern(/^[\p{L}\p{N}\s]+$/u).min(3).max(255),
-                email: Joi.string().email(),
                 password: Joi.string(),
+                replier: Joi.string().email().max(100),
+                sender: Joi.string().email().max(100),
+                title: Joi.string().pattern(/^[\p{L}\p{N}\s]+$/u).min(3).max(255),
+                url: Joi.string(),
             });
-            
-            let test;
 
             for (const check in data) {
                 if (match.describe().keys[check]) {
                     await match.validateAsync({ [check]: data[check] }).catch(error => { throw new Error (`invalide type: ${check}`); });
+                } else if (check === 'requirements' && typeof(data[check]) !== 'object') {
+                    return { success: false, message: "Invalid type: requirements" };
                 };
             }
 
